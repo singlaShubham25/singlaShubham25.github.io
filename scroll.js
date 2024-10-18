8f5d8467d82fd6b128baa9ff08a9672885525538
Shubham <shubhamsingla2503@gmail.com>
2024-10-19 01:41:53 +0530

    new portfolio

--- scroll.js
+++ scroll.js
@@ -1,11 +1,8 @@
 // animation while scrolling
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add(`show`);
-    } else {
-      entry.target.classList.remove(`show`);
     }
   });
 });
@@ -14,25 +11,37 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 const observer1 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add(`show1`);
-    } else {
-      entry.target.classList.remove(`show1`);
     }
   });
 });
 const hiddenElements1 = document.querySelectorAll(`.hidden1`);
 hiddenElements1.forEach((el) => observer1.observe(el));
 
-// nav bar remove while scrolling
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0px";
-  } else {
-    document.getElementById("navbar").style.top = "-70px";
+// tab links button
+let tabLinks = document.querySelectorAll(".links");
+let tabContents = document.querySelectorAll(".content");
+
+function opentab(tabName) {
+  for (tablink of tabLinks) {
+    tablink.classList.remove("active-link");
   }
-  prevScrollpos = currentScrollPos;
-};
+  for (tabcontent of tabContents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
+}
+
+// // nav bar remove while scrolling
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0px";
+//   } else {
+//     document.getElementById("navbar").style.top = "-70px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
